6e23fe01ef772af1bcef35ff7f68f41bf3471709
Minucia <dev@minucia.pt>
2025-02-20 18:56:39 +0000

    feat: First commit, app created

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Dev Todo List App</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 36,
+    color: "grey",
+    borderWidth: 1,
+    padding: 12,
+    borderRadius: 8,
+  },
+});
